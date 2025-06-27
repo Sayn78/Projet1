225397f0ee466259cb67f2f09a5bb7853a68ff19
Sayn78 <87267809+Sayn78@users.noreply.github.com>
2025-06-27 17:11:43 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,23 @@
 provider "aws" {
-  region = "eu-west-3" #Paris
+  region = "eu-west-3"  # Paris
 }
 
+# Utiliser un data source pour vérifier si le groupe de sécurité existe déjà
+data "aws_security_group" "existing_web_sg" {
+  filter {
+    name   = "group-name"
+    values = ["web_sg"]
+  }
+
+}
+
+# Créer le groupe de sécurité uniquement s'il n'existe pas
 resource "aws_security_group" "web_sg" {
+  count = length(data.aws_security_group.existing_web_sg.id) == 0 ? 1 : 0  # Créer le groupe de sécurité uniquement s'il n'existe pas
+
   name        = "web_sg"
   description = "Allow HTTP and SSH access"
-
+  
   ingress {
     from_port   = 22
     to_port     = 22
@@ -32,7 +44,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04ec97dc75ac850b1"
   instance_type = "t2.micro"
   key_name      = "sshsenan"
-  security_groups = [aws_security_group.web_sg.name]
+  security_groups = [aws_security_group.web_sg[0].name]  # Utiliser le groupe de sécurité créé ou trouvé
 
   tags = {
     Name = "web_server1"
