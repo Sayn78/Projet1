7607416119c7fc3445949a7dcc1aeb0f510bbd46
Sayn78 <anthony.senan@laposte.net>
2025-07-09 07:16:35 +0200

    configuration Jest

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,22 @@
 import js from "@eslint/js";
+import pluginJest from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
   {
     ignores: ["node_modules", "dist"], // ✅ nouvelle façon d’ignorer des fichiers
     files: ["**/*.js", "**/*.ts"],
+    plugins: {
+      jest: pluginJest
+    },
+    languageOptions: {
+      globals: {
+        ...pluginJest.environments.globals.globals
+      }
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"]
     }
   }
 ];
-
