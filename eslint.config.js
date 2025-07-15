e9fb2f9899b5b2e73c41c72bdd18c09334712576
Sayn78 <anthony.senan@laposte.net>
2025-07-15 09:36:20 +0200

    Formating & Linting

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,17 @@ import pluginJest from "eslint-plugin-jest";
 
 export default [
   js.configs.recommended,
-  pluginJest.configs.recommended,
   {
+    files: ["tests/**/*.js"], // Scopes les fichiers JS dans le dossier tests
     ignores: ["node_modules", "dist"],
-    files: ["**/*.js", "**/*.ts"],
+    plugins: {
+      jest: pluginJest
+    },
+    languageOptions: {
+      globals: {
+        ...pluginJest.environments.globals.globals
+      }
+    },
     rules: {
       semi: ["error", "always"],
       quotes: ["error", "double"]
