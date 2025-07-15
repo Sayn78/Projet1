3b49003887f34e55cc6ed73289c29d2ad1ab14e9
Sayn78 <anthony.senan@laposte.net>
2025-07-15 09:58:38 +0200

    Formating & Linting

--- configs/jest.config.mjs
+++ configs/jest.config.mjs
@@ -1,4 +1,10 @@
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default {
-  testMatch: ["**/tests/**/*.test.js"], 
+  rootDir: resolve(__dirname, '..'),
+  testMatch: ['**/tests/**/*.test.js'],
   transform: {},
 };
